37cab9d68a9da822fe8c9f92897980ebe3924cb2
therightcafe <61467666+therightcafe@users.noreply.github.com>
2020-05-07 12:08:06 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Right Values REVAMPED
+Right Values REVAMPED is a quiz for people on the right wing of the political spectrum, based off of rightvalues. This version contains external sources on ideologies, adds ideologies to the list of results, changes and renames some ideologies, has new scales in each axis and contains a focus on more historic idelogical movements, rather than ideologies in the abstract.
---
+++ questions.js
@@ -0,0 +1,823 @@
+questions = [
+    //preserve (Conserve) vs Reform 
+    {
+        "question": "While not perfect, right wing political parties are useful means to an end.",
+        "i18n": {
+        },
+        "effect": {
+            "a": -5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Incremental change through subversion is better than full scale rebellion.",
+        "i18n": {
+        },
+        "effect": {
+            "a": -5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "We should not arrest people for using recreational drugs.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Abortion should be legalized.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "We must push reform, not mindless revolution!",
+        "i18n": {
+        },
+        "effect": {
+            "a": -5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Promotion of High Culture is a necesity.",
+        "i18n": {
+        },
+        "effect": {
+            "a": -5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "We must use pragmatism when changing politics, not Wignattery.",
+        "i18n": {
+        },
+        "effect": {
+            "a": -5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Tradition tends to hold societies back.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 10,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+     {
+        "question": "I take no issue with social liberalism.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 10,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+//Stateown vs Market
+    {
+        "question": "A Laissez-Faire economy leads to monopoly and degenerate industries.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 10,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "We ought to reduce regulations on industry.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": -5,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Opression by corporations aren't a valid thing, because employees voluntarily work.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": -5,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Regulation reduces the positive effects of Capitalism.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": -5,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Most rich people got their wealth through merit.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": -5,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "We do not need to punish the rich to elevate the common man.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": -5,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "We should have a minimal government alongisde with a fully functional market.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": -5,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "It is necessary for the government to intervene in the economy to protect consumers.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "We need to have more generous safety nets.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "We should have higher taxes in exchange for better welfare.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Instead of welfare, people should learn to pull themselves up by their own bootstraps.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": -5,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Single Payer healthcare is desirable compared to privatized care.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+//Central (Authority) vs Decentral (Liberty)
+    {
+        "question": "Conservatism is about order and tradition, NOT liberty.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 5,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Anarchism cannot exist perpetually.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 5,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Hereditary or Appointed succession is better than election.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 5,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Stronger government needs to be curbed to prevent tyrannny.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -5,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "I would rather live in Francoist Spain than in an ancap society.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 5,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Democracy ought to be done away with, as it makes politics uninnovative.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 5,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "I prefer a Venetian Doge esque system to an absolute monarch like Louis XIII.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -5,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+     {
+        "question": "I want a absolute monarch.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 5,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "If there has to be a state, I would choose the old American Republic to the Ancien Regime.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -5,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+//Nation (Morality) vs World (Peace)
+    {
+        "question": "My nation's ideals are downright better than most other nations on earth.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": -5,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Rather than bombing innocent countries, we should primarily work on peace.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 5,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Countries with authoritarian regimes should be toppled.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": -5,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Alliances are beneficial.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 5,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "We should focus more on issues outside of our nation.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 5,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "The enemy of my enemy is my friend.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 5,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+     {
+        "question": "Immigration should be easier than it is now.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": -5,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Having too much refugees lead to frequent terrorist attacks.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": -5,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Illegal immigration is morally wrong in most cases.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": -5,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Illegal immigrants and regular citizens deserve the same treatments.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 5,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+//Religious vs Secular
+    {
+        "question": "My religion should be spread as much as possible.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 5,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Theocracy is a good idea.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 5,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Religion is just fiction for humans to develop.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": -5,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Religion should play a much smaller role compared to the present.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": -5,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Churches should be taxed.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": -5,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Religious organizations often provide the better help compared to other non-profits.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 5,
+            "f": 0,
+            "g": 0
+        }
+    },
+//Industry vs Nature
+    {
+        "question": "A rapid technological advancement causes more bad than good.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": -5,
+            "g": 0
+        }
+    },
+    {
+        "question": "Ted Kaczynski was right!",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": -5,
+            "g": 0
+        }
+    },
+    {
+        "question": "We should accelerate our technological progress.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 5,
+            "g": 0
+        }
+    },
+    {
+        "question": "Rather than trying to restrict factories, we should let technology advance and find a easier solution in the future.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 5,
+            "g": 0
+        }
+    },
+    {
+        "question": "Decelerating our society could mean a harder life, but it's overall worth it considering the happiness decelerating would bring.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": -5,
+            "g": 0
+        }
+    },
+    {
+        "question": "The more we accelerate, the more risks there is.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": -5,
+            "g": 0
+        }
+    },
+//Race vs Equality
+    {
+        "question": "Each races has their own strengths and weaknesses.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 5
+        }
+    },
+    {
+        "question": "My race is suprerior to other races, or vice versa.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 10
+        }
+    },
+    {
+        "question": "We should judge people by their character rather than their race.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": -5
+        }
+    },
+    {
+        "question": "Racism is a real issue.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": -5
+        }
+    },
+    {
+        "question": "Affirmative action is good.",
+        "i18n": {
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 5
+        }
+    }
+    
+
+];
